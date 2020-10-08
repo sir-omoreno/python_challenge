0bde501e7ff0c19fdc111a403753d7c2ca698fe2
sir-omoreno <ozmoreno@gmail.com>
2020-10-08 01:52:38 -0400

    PyPoll update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,18 +5,43 @@
 
 import os
 import csv
-import statistics
+from collections import Counter
 
 # Creating variables for counters
 
-candidates = 0
-total_votes = 0
-candidate_1 = 0
-candidate_2 = 0
-candidate_3 = 0
-candidate_4 = 0
-total_tallly = []
+votes = []
+#votes_counter = []
+
 
 # Opening the csv file for analysis
 
-csvpath = os.path.join(r'C:\Users\Oswaldo Moreno\Desktop\homework\python_challenge\PyPoll\election_data.csv')
+csvpath = os.path.join(os.path.dirname(__file__), 'election_data.csv')
+
+with open(csvpath) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    #Remove the Header from the document
+    csv_header = next(csv_reader)
+
+    for i in csv_reader:
+        votes.append(str(i[2]))
+    
+vote_count = Counter(votes)
+
+max_votes = max(vote_count.values())
+
+
+winner = [i for i in vote_count.keys() if vote_count[i] == max_votes]
+
+    
+
+print("Election Results")
+print("----------------------------------------------------------")
+print((f"Total Votes: {len(votes)}"))
+print("----------------------------------------------------------")
+# print(f"Correy: {(Correy_percent)} % \t Numbers of votes: {Correy_votes}")
+# print(f"Khan: {(Khan_percent)} % \t \t Numbers of votes: {Khan_votes}")
+# print(f"Li: {(Li_percent)} % \t \t Numbers of votes: {Li_votes}")
+# print(f"O'Tooley: {(Otooley_percent)} % \t Numbers of votes: {Otooley_votes}")
+# print("----------------------------------------------------------")
+# print("----------------------------------------------------------")
+# print(f"Winner: {key} !! .... Congratulations!! ")
